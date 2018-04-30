ed0c92a9d317af9718561b865406ef61d7f779e6
DuParkLinux <dupark3@gmail.com>
2018-04-30 08:11:22 -0400

    First working version of build_max_heap

--- main.cpp
+++ main.cpp
@@ -23,11 +23,14 @@ static const int size = 10;
 using namespace std;
 
 void PrintArray(const array<int, size>& arr){
-    cout << '{' << arr[0];
-    for(size_t i = 1; i!= size; ++i){
+    cout << "             " << arr[0] << endl
+         << "         " << arr[1] << "        " << arr[2] << endl
+         << "     " << arr[3] << "   " << arr[4] << "   " << arr[5] << "  " << arr[6] << endl
+         << "   " << arr[7] << "    " << arr[8] << "   " << arr[9] << endl;
+    /*cout << '{' << arr[0];
+    for(size_t i = 1; i!= size; ++i)
         cout << ", " << arr[i];
-    }
-    cout << '}' << endl;
+    cout << '}' << endl;*/
 }
 
 void swap(array<int, size>& nums, int first_index, int second_index){
@@ -39,38 +42,44 @@ void swap(array<int, size>& nums, int first_index, int second_index){
 void max_heapify(array<int, size>& nums, int n){
     int left_child_index = 2 * n + 1;
     int right_child_index = 2 * n + 2;
+
     int left_child = INT_MIN;
     int right_child = INT_MIN;
+
     if (left_child_index < size)
         left_child = nums[left_child_index];
     if (right_child_index < size)
         right_child = nums[right_child_index];
 
-    cout << "Left: " << left_child << endl << "Right: " << right_child << endl;
-
     if (nums[n] < left_child && left_child > right_child){
         swap(nums, n, left_child_index);
-    } 
+        max_heapify(nums, left_child_index);
+    }
     if (nums[n] < right_child){
         swap(nums, n, right_child_index);
+        max_heapify(nums, right_child_index);
     }
 }
 
 void build_max_heap(array<int, size>& nums){
     int n = size;
-    max_heapify(nums, (size - 1) / 2);
+    for(int i = (size - 1) / 2; i >= 0; --i){
+        max_heapify(nums, i);
+        cout << i << " passed into max_heapify : " << endl;
+        PrintArray(nums);
+    }
 }
 
 
 int main() {
     array<int, size> nums = {5, 3, 2, -15, 0, -5, 12, 7, 10, 9};
     
-    cout << "Before: ";
+    cout << "Before: " << endl;
     PrintArray(nums);
 
     build_max_heap(nums);
 
-    cout << "After : ";
+    cout << "After : " << endl;
     PrintArray(nums);
 
     return 0;
